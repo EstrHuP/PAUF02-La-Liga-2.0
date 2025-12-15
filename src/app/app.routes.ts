5cef8035615e6240666d1661d46401efb10db098
EstrHuP <estrhup@gmail.com>
2025-12-16 00:48:20 +0100

    feat(competitions): Add competitions page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomePage } from './features/home/pages/home-page/home-page';
 import { TeamsPage } from './features/teams/pages/teams-page/teams-page';
 import { ResultsPage } from './features/results/pages/results-page/results-page';
+import { ClasificationsPage } from './features/clasifications/pages/clasifications-page/clasifications-page';
 
 export const routes: Routes = [
     // HOME
@@ -26,5 +27,13 @@ export const routes: Routes = [
         redirectTo: 'results',
         pathMatch: 'full'
     },
-    { path:'results', component: ResultsPage }
+    { path:'results', component: ResultsPage },
+
+    // CLASIFICATIONS
+    {
+        path: '',
+        redirectTo: 'clasifications',
+        pathMatch: 'full'
+    },
+    { path:'clasifications', component: ClasificationsPage }
 ];
