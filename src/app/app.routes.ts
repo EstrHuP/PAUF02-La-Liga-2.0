90f3a0c5fbae7e88a500906d233b301c3903e16e
EstrHuP <estrhup@gmail.com>
2025-12-15 22:13:51 +0100

    feat(teams): Add teams page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,21 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './features/home/pages/home-page/home-page';
+import { TeamsPage } from './features/teams/pages/teams-page/teams-page';
 
 export const routes: Routes = [
+    // HOME
     {
         path: '',
         redirectTo: 'home',
         pathMatch: 'full'
     },
-    { path:'home', component: HomePage }
+    { path:'home', component: HomePage },
+
+    // TEAMS
+    {
+        path: '',
+        redirectTo: 'teams',
+        pathMatch: 'full'
+    },
+    { path:'teams', component: TeamsPage }
 ];
