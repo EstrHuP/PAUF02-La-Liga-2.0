51b49a897ce172c559e2a2c9a4868cd4578ac276
EstrHuP <estrhup@gmail.com>
2025-12-15 14:02:10 +0100

    chore(shared): Footer and Navbar navigation

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
   templateUrl: './footer.html',
-  styleUrl: './footer.scss',
+  styleUrls: ['./footer.scss']
 })
-export class Footer {
 
-}
+export class Footer {}
